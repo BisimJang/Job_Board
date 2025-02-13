418a5a8cf1905e7c83c5682e93470b74f595d405
BisimJang <jangjason27@gmail.com>
2025-02-13 10:46:26 +0100

    This is a basic job board that uses Django

---
+++ job_board/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import index, job_detail
+
+
+urlpatterns = [
+    
+    path('', index, name='home'),
+    path('job/<int:pk>/', job_detail, name='job-detail'),
+]
